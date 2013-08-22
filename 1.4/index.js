9200f4abdebd6a2b172586164cbcae34aa093c60
jianping.xwh <jianping.xwh@taobao.com>
2013-08-22 11:15:30 +0800

    add sync.

--- 1.4/index.js
+++ 1.4/index.js
@@ -6,6 +6,7 @@ KISSY.add(function (S, Node, MenuButton, Menu, undefined) {
     var $ = Node.all;
     var Select = MenuButton.Select;
     var PREFIX_CLS = 'bf-';
+    var Item = Menu.Item;
     function ButterflySelect(target,config){
         var self = this;
         var config = self._decorate(target,config);
@@ -15,40 +16,62 @@ KISSY.add(function (S, Node, MenuButton, Menu, undefined) {
         self.set('target',target);
     }
     S.extend(ButterflySelect,Select,{
-        _decorate:function(element, cfg){
-            element = S.one(element);
+        /**
+         * 处理menu的配置
+         * @param cfg
+         * @return {*}
+         * @private
+         */
+        _decorate:function(element,cfg){
+            var self = this;
+            element = $(element);
+            if(!element.length) return false;
             cfg = cfg || {};
             cfg.elBefore = element;
 
-            var allItems = [],
-                selectedItem = null,
-                curValue = element.val(),
-                options = element.all("option");
+            var menuData = self.menuData(element);
+            S.mix(cfg, {
+                menu: S.mix(menuData.menu, cfg.menuCfg)
+            });
+
+            S.mix(cfg, menuData.selectedItem);
+            return cfg;
+        },
+        /**
+         * 从select中拉取数据，生成menu可用的data
+         * @return {Object} { selectedItem:{}, allItems : [] }
+         */
+        menuData:function(element){
+            var self = this;
+            var element = element || self.get('target');
+            var curValue = element.val();
+            var options = element.all("option");
+            var data = {
+                //当前选中的选项
+                selectedItem:{},
+                //所有的菜单项
+                menu:{
+                    children: []
+                }
+            };
 
             options.each(function (option) {
                 var item = {
                     xclass: 'option',
                     content: option.text(),
                     elCls: option.attr("class"),
-                    value: option.val()
+                    value: option.val(),
+                    prefixCls:PREFIX_CLS
                 };
                 if (curValue == option.val()) {
-                    selectedItem = {
+                    data.selectedItem = {
                         content: item.content,
                         value: item.value
                     };
                 }
-                allItems.push(item);
+                data.menu.children.push(item);
             });
-
-            S.mix(cfg, {
-                menu: S.mix({
-                    children: allItems
-                }, cfg.menuCfg)
-            });
-
-            S.mix(cfg, selectedItem);
-            return cfg;
+            return data;
         },
         /**
          * 运行组件
@@ -67,6 +90,28 @@ KISSY.add(function (S, Node, MenuButton, Menu, undefined) {
                     $target.fire('change');
                     self.fire("change",{value:e.newVal,$select:$target});
                 })
+            })
+        },
+        /**
+         * 模拟选择框同步原生select的数据
+         * @return {*}
+         */
+        sync:function(){
+            var self = this;
+            var menu = self.get("menu");
+            if(!menu) return self;
+            //删除所有的下拉项
+            self.removeItems(true);
+            var menuData = self.menuData();
+            var selectedItem = menuData.selectedItem;
+            var items = menuData.menu.children;
+            S.each(items,function(itemConfig){
+                var item = new Item(itemConfig);
+                self.addItem(item);
+                if(itemConfig.value == selectedItem.value){
+                    self.set('value',itemConfig.value);
+                    self.set('content',itemConfig.content);
+                }
             })
         }
     },{ATTRS:{
